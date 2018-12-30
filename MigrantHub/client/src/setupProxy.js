5738f09b73b2185a96a4e2fdb1055e6ab131728a
Alexandre Masmoudi <a-masmoudi@hotmail.com>
2018-12-30 13:17:20 -0500

    Setting Proxy to external IP

--- MigrantHub/client/src/setupProxy.js
+++ MigrantHub/client/src/setupProxy.js
@@ -2,6 +2,6 @@ const proxy = require('http-proxy-middleware');
 require('dotenv').config();
 
 module.exports = function (app) {
-  app.use(proxy('/api/', { target: process.env.PROXY || "http://35.203.28.214:5000" }));
-  app.use(proxy('/uploads/', { target: process.env.PROXY || "http://35.203.28.214:5000" }));
+  app.use(proxy('/api/', { target: "http://35.203.28.214:5000" || process.env.PROXY }));
+  app.use(proxy('/uploads/', { target: "http://35.203.28.214:5000" || process.env.PROXY }));
 };
